48eec80b2799b997c6f245e0e34e86f977cd6511
snow-zxr <c.snow0908@gmail.com>
2021-10-24 11:05:40 +0000

    first

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,30 @@
 <script>
 	import Header from '$lib/Header.svelte';
+	import Section from '$lib/Section.svelte';
+
+	let myInfo = {
+    title: "Info",
+    item1: "Snow Chow",
+    item2: 13,
+    item3: "Chung Ling Private High School"
+	}
+
+	let myHobby = {
+    title: "Hobby",
+    item1: "Swimming",
+    item2: "Listen to Music",
+    item3: "Watch Movies"
+	}
+
+	let myPersonality = {
+    title: "Personality",
+    item1: "Queit",
+    item2: "Humble",
+    item3: "Kind"
+	}
 </script>
 
 <Header title={"My Biography"} />
+<Section {...myInfo} />
+<Section {...myHobby} />
+<Section {...myPersonality} />
