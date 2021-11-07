7c89f95248bc641790f0fafeb0914a9df1789f89
snow-zxr <c.snow0908@gmail.com>
2021-11-07 09:06:56 +0000

    first

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,22 +9,23 @@ item2: 13,
 item3: "Chung Ling Private High School"
 }
 
-Let myhobby = {
-title: "Hobby"
+let myHobby = {
+title: "Hobby",
 item1: "Swimming",
 item2: "Listen to Music",
 item3: "Watch Movies"
 }
 
-Let myPersonality = {
-title: "personality".
-item1: "Queit".
+let myPersonality = {
+title: "personality",
+item1: "Queit",
 item2: "Humble",
 item3: "Kind"
 }
 
 </script>
-<Header title={"my Biography"} />
+
+<Header title={"My Biography"} />
 <section {...myInfo} />
-<section {...myHobby} /
+<section {...myHobby} />
 <section {...myPersonality} />
